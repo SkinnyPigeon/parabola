a9c0fe255636fb449648a15ed82b4f2fa632e00c
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-08 23:16:57 +0100

    not exactly the initial commit

---
+++ conversion.rb
@@ -0,0 +1,54 @@
+class Numeric
+
+  def degrees
+   self * Math::PI / 180
+  end
+
+  def horizontal( speed, angle)
+    answer = speed * Math::cos( angle.degrees )
+    return answer
+  end
+
+  def vertical( speed, angle )
+    answer = speed * Math::sin( angle.degrees )
+    return answer
+  end
+
+
+
+end
+
+calc = Numeric.new
+
+
+h = calc.horizontal( 50, 60 )
+puts h
+
+v = calc.vertical( 50, 60 )
+puts v
+
+t = 4
+g = 9.8
+
+height = calc.vertical( 50, 60 ) * t - 0.5 * g * t**2
+puts height
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ maths.rb
@@ -0,0 +1,33 @@
+# t = time
+# y = height
+# x = distance
+# V = vertical velocity
+# V0 = initial vertical velocity 
+# hv = horizontal velocity (is a constant unless acted on by an external force such a wind)
+# g = gravitational acceleration
+
+
+#Throwing a ball at aan angle of 45 degrees and 20 meters per second
+
+# horizontal component
+# hv = U0
+# x = hv * t
+
+# vertical component
+# V = V0 - g t                V = calc.vertical - g * t
+# y = V0t - .5 g t**2         y = calc.vertical * t - .5 * g * t**2 
+
+# highest point
+# V = 0
+# t = V0 / g
+# h = .5 V0 ** 2 / g
+
+# at ground impact
+# y = 0
+# t = 2 V0 / g
+# V = -V0
+
+
+
+t = 4
+g = 9.8*t
